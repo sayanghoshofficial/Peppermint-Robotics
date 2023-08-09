6d81b51eb3120ef06538f6b67c2325f3bfefffba
Sayan Ghosh <sayanghosh379@gmail.com>
2023-08-09 20:44:51 +0530

    deploy the project

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/peppermint-robotics">
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
